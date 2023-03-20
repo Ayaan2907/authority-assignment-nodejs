76623dd171d7d8b7a20e49c8359b2fa7660b3c87
Ayaan2907 <ayaanansari7921@outlook.com>
2023-03-20 17:44:49 +0530

    added email

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ dotenv.config();
 const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT || 8000;
 
-
 const defaultConfig = {
     baseUrl: "https://rest.gohighlevel.com/v1",
-  headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${API_KEY}`,
-  },
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+    },
 };
 
 axios.defaults.baseURL = defaultConfig.baseUrl;
 axios.defaults.headers = defaultConfig.headers;
 
-
 app.post("/run", async (req, res) => {
-  console.log("running")
-  let resp = {};
-  const contactId = req.query.contactId;
-  const customFieldId = req.query.customeFieldId;
-  const value = req.query.value;
+    console.log("running");
+    let resp = {};
+    const contactId = req.query.contactId;
+    const customFieldId = req.query.customeFieldId;
+    const value = req.query.value;
+    const email = req.query.email;
 
-  const data = {
-    customFields: [
-      {
-        id: customFieldId,
-        value: value,
-      },
-    ],
-  };
+    const data = {
+        email: email,
+        customFields: [
+            {
+                id: customFieldId,
+                value: value,
+            },
+        ],
+    };
     try {
         const response1 = await axios.get(`contacts/${contactId}`);
         resp.contact = response1.data?.contact;
@@ -49,21 +49,17 @@ app.post("/run", async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-  res.send(resp).status(200)
-  // res.send({
-  //   data: data,
-  //   req: req.body,
-  //   params: req.query,
-  // });
-
+    res.send(resp).status(200);
+    // res.send({
+    //   data: data,
+    //   req: req.body,
+    //   params: req.query,
+    // });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
-
 
 // const contactId = "pyytrTDvGcaplNjF25sX";
 // const customFieldId = "8Ce8a0bFshmJ4mRQxEpc";
@@ -76,44 +72,43 @@ app.listen(PORT, () => {
 //     ],
 // };
 
+// const getallContacts = async () => {
+//     try {
+//         const response = await axios.get("contacts");
+//         contactId = response.data.contacts[0].id;
+//         console.log(`Fetching contact with id: ${contactId}`);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
-  // const getallContacts = async () => {
-  //     try {
-  //         const response = await axios.get("contacts");
-  //         contactId = response.data.contacts[0].id;
-  //         console.log(`Fetching contact with id: ${contactId}`);
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // };
+// const getContactById = async (ID) => {
+//     try {
+//         const response = await axios.get(`contacts/${ID}`);
+//         console.log(response.data?.contact);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
-  // const getContactById = async (ID) => {
-  //     try {
-  //         const response = await axios.get(`contacts/${ID}`);
-  //         console.log(response.data?.contact);
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // };
+// const getCustomFieldById = async (ID) => {
+//     try {
+//         const response = await axios.get(`custom-fields/${ID}`);
+//         console.log(JSON.stringify(response.data));
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
-  // const getCustomFieldById = async (ID) => {
-  //     try {
-  //         const response = await axios.get(`custom-fields/${ID}`);
-  //         console.log(JSON.stringify(response.data));
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // };
-
-  // const updateContact = async (ID, data) => {
-  //     try {
-  //         const response = await axios.put(`contacts/${ID}`, data);
-  //         console.log(response.data?.contact?.customField);
-  //         console.log("Updated");
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // };
+// const updateContact = async (ID, data) => {
+//     try {
+//         const response = await axios.put(`contacts/${ID}`, data);
+//         console.log(response.data?.contact?.customField);
+//         console.log("Updated");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
 // async function main() {
 //     await getallContacts();
